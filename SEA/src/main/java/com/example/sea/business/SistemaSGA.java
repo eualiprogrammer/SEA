4c3ecbd3079ff3a9ed9b1b4a0fe70a3ea612a301
Alícia Maria <eualiprogrammer@gmail.com>
2025-12-04 04:14:17 -0300

    Update SistemaSGA.java

--- SEA/src/main/java/com/example/sea/business/SistemaSGA.java
+++ SEA/src/main/java/com/example/sea/business/SistemaSGA.java
@@ -1,29 +1,31 @@
 package com.example.sea.business;
 
 public class SistemaSGA {
-    
+
     private static SistemaSGA instance;
-    
-    private IControladorPalestrante controladorPalestrante;
+
     private IControladorSala controladorSala;
+    private IControladorPalestrante controladorPalestrante;
     private IControladorParticipante controladorParticipante;
     private IControladorEvento controladorEvento;
     private IControladorPalestra controladorPalestra;
     private IControladorWorkshop controladorWorkshop;
     private IControladorInscricao controladorInscricao;
     private IControladorCertificado controladorCertificado;
-   
+
+    private IControladorNotificacao controladorNotificacao;
+
     private SistemaSGA() {
-        System.out.println("Inicializando o Sistema SGA...");
-        
-        this.controladorPalestrante = new ControladorPalestrante();
         this.controladorSala = new ControladorSala();
+        this.controladorPalestrante = new ControladorPalestrante();
         this.controladorParticipante = new ControladorParticipante();
         this.controladorEvento = new ControladorEvento();
         this.controladorPalestra = new ControladorPalestra();
         this.controladorWorkshop = new ControladorWorkshop();
         this.controladorInscricao = new ControladorInscricao();
         this.controladorCertificado = new ControladorCertificado();
+        
+        this.controladorNotificacao = new ControladorNotificacao();
     }
 
     public static SistemaSGA getInstance() {
@@ -33,35 +35,14 @@ public class SistemaSGA {
         return instance;
     }
 
-    public IControladorPalestrante getControladorPalestrante() {
-        return controladorPalestrante;
-    }
-
-    public IControladorSala getControladorSala() {
-        return controladorSala;
-    }
-
-    public IControladorParticipante getControladorParticipante() {
-        return controladorParticipante;
-    }
-
-    public IControladorEvento getControladorEvento() {
-        return controladorEvento;
-    }
-
-    public IControladorPalestra getControladorPalestra() {
-        return controladorPalestra;
-    }
-
-    public IControladorWorkshop getControladorWorkshop() {
-        return controladorWorkshop;
-    }
-
-    public IControladorInscricao getControladorInscricao() {
-        return controladorInscricao;
-    }
-
-    public IControladorCertificado getControladorCertificado() {
-        return controladorCertificado;
-    }
+    // Getters
+    public IControladorSala getControladorSala() { return controladorSala; }
+    public IControladorPalestrante getControladorPalestrante() { return controladorPalestrante; }
+    public IControladorParticipante getControladorParticipante() { return controladorParticipante; }
+    public IControladorEvento getControladorEvento() { return controladorEvento; }
+    public IControladorPalestra getControladorPalestra() { return controladorPalestra; }
+    public IControladorWorkshop getControladorWorkshop() { return controladorWorkshop; }
+    public IControladorInscricao getControladorInscricao() { return controladorInscricao; }
+    public IControladorCertificado getControladorCertificado() { return controladorCertificado; }
+    public IControladorNotificacao getControladorNotificacao() { return controladorNotificacao; }
 }
