81fa5251b539205a174fec70f1d020b81096a3ea
Alícia Maria <eualiprogrammer@gmail.com>
2025-12-03 23:48:49 -0300

    application

--- SEA/src/main/java/com/example/sea/Application.java
+++ SEA/src/main/java/com/example/sea/Application.java
@@ -1,5 +1,6 @@
 package com.example.sea;
 
+import com.example.sea.gui.ScreenManager;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -9,10 +10,16 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        ScreenManager.getInstance().setPrimaryStage(stage);
+
+        stage.setWidth(1280);
+        stage.setHeight(720);
+        stage.setResizable(false);
+
+        ScreenManager.getInstance().carregarTela("TelaLogin.fxml", "Login");
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
